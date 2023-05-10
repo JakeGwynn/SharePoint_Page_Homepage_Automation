7853924ef9e7fc2ca0eeca3bc8711e32375e045e
Jake Gwynn <74085669+JakeGwynn@users.noreply.github.com>
2023-05-10 12:37:44 -0700

    Update Deploy-PagesAndHomePage.ps1

--- Deploy-PagesAndHomePage.ps1
+++ Deploy-PagesAndHomePage.ps1
@@ -7,25 +7,32 @@ param(
     [string]$HomePageName
 )
 
+# Set Variable for Credential object name
+$CredentialName = "AppRegistration"
+
+# Retrieve the automation account credentials
+$Cred = Get-AutomationPSCredential -Name $CredentialName
+$AppId = $Cred.UserName
+$ClientSecret = $Cred.GetNetworkCredential().Password
+
 # Retrieve the automation account variables
 $siteTemplateSharePointSiteUrl = Get-AutomationVariable -Name "SiteTemplateSharePointSiteUrl"
 $siteTemplateSharePointDocumentLibraryName = Get-AutomationVariable -Name "SiteTemplateSharePointDocumentLibraryName"
 
 # Connect to the SharePoint site
-Connect-PnPOnline -Url $siteTemplateSharePointSiteUrl -Interactive
+Connect-PnPOnline -Url $siteTemplateSharePointSiteUrl -ClientId $AppId -ClientSecret $ClientSecret
 
 # Download the site template file to temp storage
 $tempFilePath = Join-Path $env:TEMP $TemplateFileName
-Get-PnPFile -ServerRelativeUrl "$siteTemplateSharePointDocumentLibraryName/$TemplateFileName" -Path $tempFilePath
+Get-PnPFile -Url "$siteTemplateSharePointDocumentLibraryName/$TemplateFileName" -Path $env:TEMP -Filename $TemplateFileName -AsFile
 
 # Connect to the target SharePoint site
-Connect-PnPOnline -Url $SharePointSiteURL -Interactive
+Connect-PnPOnline -Url $SharePointSiteURL -ClientId $AppId -ClientSecret $ClientSecret
 
 # Deploy the site template file
 Invoke-PnPSiteTemplate -Path $tempFilePath
 
 # Set the home page
-Set-PnPHomePage -RootFolderRelativeUrl $HomePageName
+# Set-PnPHomePage -RootFolderRelativeUrl $HomePageName
 
-# Clean up temp file
-Remove-Item -Path $tempFilePath
+Disconnect-PnPOnline
